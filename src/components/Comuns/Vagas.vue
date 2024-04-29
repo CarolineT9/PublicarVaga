099a01bf1a1676909544ceaf1b6dca40d8df530f
CarolineT9 <caroltvrs@gmail.com>
2024-04-29 19:26:49 -0300

    inserindo dados no localstorage

---
+++ src/components/Comuns/Vagas.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="row mt-5">
+      <div class="col">
+        <div class="card">
+          <div class="card-header bg-dark text-">{{titulo}}</div>
+          <div class="card-body">
+            <p>{{descricao}}</p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Salario: {{ salario }} | Modalidade: {{modalidade}} | Tipo: {{tipo}} | Publicação: {{ publicacao }} </small>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Vagas',
+    props: ['titulo', 'descricao', 'salario', 'modalidade', 'tipo', 'publicacao']
+}
+</script>
+
+<style>
+
+</style>
--- src/components/Views/PublicarVagas.vue
+++ src/components/Views/PublicarVagas.vue
@@ -1,36 +1,86 @@
 <template>
-    <div class="container">
-        <div class="h-100 bg-light p-5 border rounded-3">
-    <h2>Pesquisar vagas</h2>
-    <div class="row">
-      <div class="col">
-        <div class="form-group">
-          <label for="">Título da vaga</label>
-          <input
-            type="text"
-            class="form-control"
-            placeholder="Pesquise por palavras chaves ex: Php, JavaScript, Sênior..."
-          />
-          <small class="form-text text-muted"
-            >Informe palavras que estejam relacionadas como título da vaga que
-            você procura.</small
-          >
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col">
-        <button class="btn btn-outline-dark mt-2" type="button">Buscar</button>
-      </div>
+<div class="container py-4">
+  <div class="row">
+    <div class="col">
+      <h4>Apresente sua vaga para milhares de profissionais e de graça</h4>
     </div>
   </div>
+  <div class="row mt-3">
+    <div class="col">
+      <label for="" class="form-label">Título da Vaga</label>
+      <input type="text" class="form-control"   v-model="titulo">
+      <div class="form-text">Por exemplo: Programador JavaScript e VueJS.</div>
     </div>
+  </div>
+  <div class="row mt-3">
+    <div class="col">
+      <label for="" class="form-label">Descrição</label>
+      <textarea type="text" class="form-control" v-model="descricao"></textarea>
+      <div class="form-text">Informe os detalhes da vaga.</div>
+    </div>
+  </div>
+  <div class="row mt-3">
+    <div class="col">
+      <label for="" class="form-label">Salário</label>
+      <input type="number" class="form-control" v-model="salario">
+      <div class="form-text">Informe o salário.</div>
+    </div>
+    <div class="col">
+      <label for="" class="form-label">Modalidade</label>
+      <select type="number" class="form-select" v-model="modalidade">
+        <option value="" disabled>Selecione</option>
+        <option value="1">Home Office</option>
+        <option value="2">Presencial</option>
+      </select>
+      <div class="form-text">Informe onde as atividades serão realizadas.</div>
+    </div>
+    <div class="col">
+      <label for="" class="form-label">Tipo</label>
+      <select type="number" class="form-select" v-model="tipo">
+        <option value="" disabled>Selecione</option>
+        <option value="1">CLT</option>
+        <option value="2">PJ</option>
+      </select>
+      <div class="form-text">Informe o tipo de contratação.</div>
+    </div>
+  </div>
+  <div class="row mt-3">
+    <div class="col">
+      {{ titulo }} {{ descricao }} {{ salario }} {{ modalidade }} {{ tipo }}
+      <button type="submit" class="btn btn-primary" @click="salvarVaga()">
+        Cadastrar
+      </button>
+    </div>
+  </div>
+
+</div>
 
 </template>
 
 <script>
 export default {
-    name: 'PublicarVagas'
+    name: 'PublicarVagas',
+    data: () =>({
+      titulo: '',
+      descricao: '',
+      salario:'',
+      modalidade: '',
+      tipo: ''  
+    }),
+    methods:{
+      salvarVaga(){
+        let vaga = {
+          titulo: this.titulo,
+          descricao: this.descricao,
+          salario: this.salario,
+          modalidade: this.modalidade,
+          tipo: this.tipo
+          
+        }
+        localStorage.setItem('vagas', JSON.stringify(vaga))
+      }
+    }
+
 }
 </script>
 
